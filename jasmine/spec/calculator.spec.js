88d150ebc2ae592d2e955020b94f2c0ef4f67c13
sebam2k4 <sebam2k4@gmail.com>
2017-10-22 15:10:29 +0100

    calculation tests all passing

--- jasmine/spec/calculator.spec.js
+++ jasmine/spec/calculator.spec.js
@@ -2,64 +2,101 @@
 
 describe("Calculator operations - Addition", function () {
     beforeEach(function() {
+        display_input = 0;
+        inputs = [];
         total = 0;
+        operation = "";
+        a = 0;
+        b = 0;
+        // create dummy element so updateDisplay() function doesn't cause
+        // TypeError: Cannot set property 'value' of null
+        var dummyElement = document.createElement('span');
+        document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(dummyElement);
     });
 
     it("should add a number", function() {
-        input = 5;
         add();
-
+        inputs = ["5"];
+        equal();
         expect(total).toEqual(5);
     });
 
-    it("should add two numbers", function() {
-        calc.add(2);
-        calc.add(1);
-        expect(calc.total).toEqual(3);
+    it("should add two numbers together", function() {
+        inputs = ["5"];
+        add();
+        inputs = ["5"];
+        equal();
+        expect(total).toEqual(10);
     });
 
     it("should add two negative numbers", function() {
-        calc.add(-5);
-        calc.add(-9);
-        expect(calc.total).toEqual(-14);  // 0 + (-5) + (-9) = -14
+        inputs = ["5"];
+        changeSign();
+        add();
+        inputs = ["5","5"];
+        changeSign();
+        equal();
+        expect(total).toEqual(-60);
     });
 
     it("should add a negative and a positive number", function() {
-        calc.add(-5);
-        calc.add(9);
-        expect(calc.total).toEqual(4);  // 0 + (-5) + 9 = 4
+        inputs = ["2"]
+        changeSign();
+        add();
+        inputs = ["2"];
+        equal();
+        expect(total).toEqual(0);
     });
 
 });
 
 describe("Calculator operations - Subtraction", function () {
-    // calc.total = 0; for all subtraction tests
+
     beforeEach(function() {
-        calc = new Calculator();
-        calc.total = 0; //calculator current total set to 0
+        display_input = 0;
+        inputs = [];
+        total = 0;
+        operation = "";
+        a = 0;
+        b = 0;
+        // create dummy element so updateDisplay() function doesn't cause
+        // TypeError: Cannot set property 'value' of null
+        var dummyElement = document.createElement('span');
+        document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(dummyElement);
     });
 
     it("should subtract a number", function() {
-        calc.subtract(10);
-        expect(calc.total).toEqual(-10);
+        subtract();
+        inputs = ["1","0"];
+        equal();
+        expect(total).toEqual(-10);
     });
 
     it("should subtract two numbers", function() {
-        calc.subtract(2);
-        calc.subtract(1)
-        expect(calc.total).toEqual(-3); // 0 - 2 - 1 = -3
+        inputs = ["5"];
+        subtract();
+        inputs = ["5"];
+        equal();
+        expect(total).toEqual(0);
     });
 
     it("should subtract two negative numbers", function() {
-        calc.subtract(-5);
-        calc.subtract(-9)
-        expect(calc.total).toEqual(14); // 0 - (-5) - (-9) = 14
+        inputs = ["5"];
+        changeSign();
+        subtract();
+        inputs = ["5"];
+        changeSign();
+        equal();
+        expect(total).toEqual(0);
     });
 
     it("should subtract a negative and a positive number", function() {
-        calc.subtract(-5); 
-        calc.subtract(9)
-        expect(calc.total).toEqual(-4); // 0 - (-5) - 9 = -4
+        inputs = ["5"];
+        changeSign();
+        subtract();
+        inputs = ["5"];
+        equal();
+        expect(total).toEqual(-10);
     });
 
 });
@@ -67,94 +104,169 @@ describe("Calculator operations - Subtraction", function () {
 describe("Calculator operations - Multiplication", function () {
     
     beforeEach(function() {
-        calc = new Calculator();
-        calc.total = 5;  //calculator current total set to 5
+        display_input = 0;
+        inputs = [];
+        total = 0;
+        operation = "";
+        a = 0;
+        b = 0;
+        // create dummy element so updateDisplay() function doesn't cause
+        // TypeError: Cannot set property 'value' of null
+        var dummyElement = document.createElement('span');
+        document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(dummyElement);
     });
 
-    it("should multiply by a number", function() {
-        calc.multiply(5);
-        expect(calc.total).toEqual(25);
+    it("should multiply two numbers together", function() {
+        inputs = ["2"];
+        multiply();
+        inputs = ["1","0"];
+        equal();
+        expect(total).toEqual(20);
     });
 
     it("should multiply by a negative number and return a negative number", function() {
-        calc.multiply(-10);
-        expect(calc.total).toEqual(-50);
+        inputs = ["2"];
+        multiply();
+        inputs = ["1","0"];
+        changeSign();
+        equal();
+        expect(total).toEqual(-20);
     });
 
     it("should return 0 when multiplying by 0", function() {
-        calc.multiply(0);
-        expect(calc.total).toEqual(0);
+        inputs = ["2","0","0"];
+        multiply();
+        inputs = ["0"];
+        equal();
+        expect(total).toEqual(0);
     });
 
     it("should multiply by a decimal number and return a decimal number", function() {
-        calc.multiply(0.1);
-        expect(calc.total).toEqual(0.5);
+        inputs = ["5"];
+        multiply();
+        inputs = [".","5"];
+        equal();
+        expect(total).toEqual(2.5);
     });
 
     it("should multiply by a decimal number and return a whole number", function() {
-        calc.multiply(0.2);
-        expect(calc.total).toEqual(1);
+        inputs = ["10"];
+        multiply();
+        inputs = [".","5"];
+        equal();
+        expect(total).toEqual(5);
     });
 
     it("should multiply by three numbers in a chain", function() {
-        calc.multiply(2);
-        calc.multiply(5);
-        calc.multiply(10)
-        expect(calc.total).toEqual(500);
+        inputs = ["10"];
+        multiply();
+        inputs = [".","5"];
+        multiply();
+        inputs = ["3"];
+        equal();
+        expect(total).toEqual(15);
     });
 });
 
 describe("Calculator operations - Division", function () {
     
     beforeEach(function() {
-        calc = new Calculator();
-        calc.total = 5;  //calculator current total set to 5
+        display_input = 0;
+        inputs = [];
+        total = 0;
+        operation = "";
+        a = 0;
+        b = 0;
+        // create dummy element so updateDisplay() function doesn't cause
+        // TypeError: Cannot set property 'value' of null
+        var dummyElement = document.createElement('span');
+        document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(dummyElement);
     });
 
     it("should divide by a number and return a whole number", function() {
-        calc.divide(5);
-        expect(calc.total).toEqual(1);
+        inputs = ["6"];
+        divide();
+        inputs = ["2"];
+        equal();
+        expect(total).toEqual(3);
     });
 
     it("should divide by a number and return a decimal number", function() {
-        calc.divide(10);
-        expect(calc.total).toEqual(0.5);
+        inputs = ["5"];
+        divide();
+        inputs = ["2"];
+        equal();
+        expect(total).toEqual(2.5);
     });
 
     it("should divide by a negative number and return a negative number", function() {
-        calc.divide(-2);
-        expect(calc.total).toEqual(-2.5);
+        inputs = ["6"];
+        divide();
+        inputs = ["2"];
+        changeSign();
+        equal();
+        expect(total).toEqual(-3);
     });
 
     it("should return an error when dividing by 0", function() {
-        calc.divide(0);
-        expect(calc.total).toBe("error! division by 0");  //change to use a message function later?
+        inputs = ["6"];
+        divide();
+        inputs = ["0"];
+        equal();
+        expect(total).toBe("error: div by 0!");
     });
 
     it("should divide by two numbers in a chain", function() {
-        calc.divide(5);
-        calc.divide(2);
-        expect(calc.total).toEqual(.5);  //change to use a message function later?
+        inputs = ["6"];
+        divide();
+        inputs = ["2"];
+        divide();
+        inputs = ["3"];
+        equal();
+        expect(total).toEqual(1);
     });
 });
 
 describe("Sample Calculation", function () {
     
     beforeEach(function() {
-        calc = new Calculator();
-        calc.total = 0;  //calculator current total set to 0
+        display_input = 0;
+        inputs = [];
+        total = 0;
+        operation = "";
+        a = 0;
+        b = 0;
+        // create dummy element so updateDisplay() function doesn't cause
+        // TypeError: Cannot set property 'value' of null
+        var dummyElement = document.createElement('span');
+        document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(dummyElement);
     });
 
-    it("Should do the following operations in order: +7, -5, +10, *3, /4, -1, *5, and return 40 as a result", function() {
-        calc.add(7);
-        calc.subtract(5);
-        calc.add(10);
-        expect(calc.total).toEqual(12);
-        calc.multiply(3);
-        calc.divide(4);
-        expect(calc.total).toEqual(9);
-        calc.subtract(1);
-        calc.multiply(5);
-        expect(calc.total).toEqual(40);
+    it("Should do the following operations in order (immediate execution logic): 3 + 7 -5 * 2 + 10 / 4 * 10 = 50 + (-25) + 5 * 2and return 60 as a result", function() {
+        inputs = ["3"];
+        add();
+        inputs = ["7"];
+        subtract();
+        inputs = ["5"];
+        multiply();
+        expect(total).toEqual(5);
+        inputs = ["2"]
+        add();
+        inputs = ["1","0"];
+        divide();
+        inputs = ["4"];
+        multiply();
+        inputs = ["1","0"];
+        equal();
+        expect(total).toEqual(50);
+        add()
+        inputs = ["2","5"];
+        changeSign();
+        add();
+        inputs = ["5"];
+        multiply();
+        inputs = ["2"];
+        equal();
+        expect(total).toEqual(60);
     });
 });
